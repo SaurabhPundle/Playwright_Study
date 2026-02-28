623af030c8e7615bd38b38e8c29b52e4c311aa9a
SaurabhPundle <pundlesaurabh@gmail.com>
2026-02-28 17:11:34 +0530

    structural changes according to framework

--- tests/Locators.spec.ts
+++ tests/Locators.spec.ts
@@ -6,7 +6,20 @@ await page.goto('https://amazon.in/');
 
 // await page.getByRole('searchbox',{name:'Search Amazon.in'}).fill('WASHING MACHINE')
 
-await page.getByLabel('Search Amazon.in').fill('laptop');
+// await page.getByLabel('Search Amazon.in').fill('laptop');
+
+// await page.locator('#twotabsearchtextbox').fill('MObilePhone');
+await page.locator('input[aria-label="Search Amazon.in"]').fill('IpHOne');
+
+await page.locator('input[aria-label="Search Amazon.in"]').press('Enter');
+
+await page.getByText('iPhone 17 Pro Max').first().click();
+const price = page.locator('span.a-price-whole:visible').textContent;
+
+// const price = page.locator('span.a-price-whole').textContent();
+console.log(await price);
+
+
 await page.waitForTimeout(10000);
 })
 
