4ab55d7b06821a72a06170d0550e9a2a368e7c0f
SaurabhPundle <pundlesaurabh@gmail.com>
2026-06-22 07:57:56 +0530

    remove faker from gitignore

---
+++ factories/user.factory.ts
@@ -0,0 +1,25 @@
+import {faker} from '@faker-js/faker'
+// used to generate random dynamic data
+export const createUser =() =>{
+    return{
+        name : faker.person.fullName(),
+        email : faker.internet.email(),
+        password : faker.internet.password(),
+
+        day : faker.number.int({min:1,max:28}).toString(),
+        month : faker.number.int({min:1,max:12}).toString(),
+        year: faker.number.int({min:1970,max:2016}).toString(),
+
+        fullName : faker.person.fullName(),
+        lastName : faker.person.lastName(),
+        company : faker.company.name(),
+
+        address1 : faker.location.streetAddress(),
+        address2 : faker.location.secondaryAddress(),
+        city : faker.location.city(),
+        state : faker.location.state(),
+        zipcode : faker.location.zipCode(),
+        mobilenumber : '9'+faker.phone.number(),
+
+    }
+}
